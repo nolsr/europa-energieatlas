bb9dc95e848e6749a0c679e40ee1a35db56fd1d8
Nils Rodenburg <niehoelzz@gmail.com>
2023-06-01 21:51:52 +0200

    established map

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
+const selected = {};
+
+
 function generateMapNodes() {
     const map = document.getElementById('map-container');
     for (let i = 0; i < 60 ** 2; i++) {
         const node = document.createElement('div');
-        node.classList.add('node');
         node.addEventListener('click', () => {
-            node.classList.toggle('active');
+            node.childNodes[0].classList.toggle('capital');
         });
         map.appendChild(node);
     }
 }
 
-const selected = {};
 function exportMap() {
     const map = document.getElementById('map-container');
     map.childNodes.forEach((n, i) => {
-        if (n.classList.contains('active')) {
+        if (n.classList.contains('capital')) {
             selected[i] = true;
         }
     });
     console.log(JSON.stringify(selected));
 }
 
+function loadExport() {
+    fetch('./assets/map.json')
+        .then((response) => response.json())
+        .then((json) => {
+            const map = document.getElementById('map-container');
+            Object.keys(json).forEach((index) => {
+                map.childNodes[index].classList.add('active');
+                const circle = document.createElement('div');
+                circle.classList.add('circle');
+                map.childNodes[index].appendChild(circle);
+            });
+        });
+}
+
 generateMapNodes();
+loadExport();
